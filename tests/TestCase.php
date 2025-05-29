a849ba66bb6bf7ec045a3e036e73922d37d9b6b7
pgonzalesmv <pgonzalesmv@gmail.com>
2025-05-29 14:30:30 -0500

    Test para validar la creación de clientes y la migracióón crear clientes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,17 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function test_se_puede_crear_un_cliente()
+{
+    $response = $this->post('/clientes', [
+        'nombre' => 'Juan Pérez',
+        'email' => 'juan@gmail.com',
+        'telefono' => '999888777',
+    ]);
+
+    $response->assertStatus(302);
+    $this->assertDatabaseHas('clientes', [
+        'email' => 'juan@gmail.com'
+    ]);
+}
 }
